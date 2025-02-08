fc0cc6d936dec1f05f965169b22b70701c90273b
Heba-Nabil <Hebanabil452@gmail.com>
2025-02-08 18:59:27 +0200

    token

--- src/components/global/NextImage.tsx
+++ src/components/global/NextImage.tsx
@@ -1,7 +1,7 @@
 import Image, { ImageProps } from "next/image";
 
-export default function NextImage(props: ImageProps) {
-  const { src, alt, ...rest } = props;
+export default function NextImage(props: any) {
+  const { src , alt, ...rest } = props;
 
-  return <Image src={src} alt={alt} {...rest} />;
+  return <img src={src} alt={alt} {...rest} />;
 }
